2024086a4bde810218463d28eae8f8b07fcd5fd9
EC2 Default User <ec2-user@ip-172-31-20-164.us-west-2.compute.internal>
2018-07-15 16:26:20 +0000

    added comments to align with blog

--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# exit the shell if any action returns a non-zero status.
 set -e
+
+# if the build directory doesn't contain pants.ini, create it
+# this will copy the FSQIO repo to persistent volume
 if [ ! -e "$FSQIO_BUILD/pants.ini" ]; then
 	sourceDir="/home/docker/download"
 	targetDir="$FSQIO_BUILD"
 	cp -a "$sourceDir/." "$targetDir"
 fi
 
+# change to the fsqio directory
 cd /home/docker/fsqio
+
+# initialize twofishes service
 ./src/jvm/io/fsq/twofishes/scripts/serve.py -p 8080 us-data/
